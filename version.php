9c2aafe37bef94c38fb16df298ac20c86b703513
Matt Donnelly <matt.donnelly@camh.ca>
2020-04-27 11:27:56 -0400

    check monitor end is 0

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_followup_email'; // Full name of the plugin (used for diagnostics).
-$plugin->version  = 2020041600;    // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2020041700;    // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2019051100;    // Requires this Moodle version.
